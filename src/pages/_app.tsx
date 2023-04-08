810e76c5f112cd8722507e837a9c3dfa7b27184c
Erick Clinton <erickleite2009321@gmail.com>
2023-04-08 12:27:10 -0300

    Adiconando estilizações globais

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+
+
+import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+export default function App({ Component, pageProps }: AppProps) {
+  
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+    
+    
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "../styles/home/styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+      
+  )
+}
